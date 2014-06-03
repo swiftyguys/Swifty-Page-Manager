924d2aa8b858541d4e0683e36b5fec13a4ec66bf
Jeroen Hoekstra <jeroen.hoekstra@longtermresults.nl>
2014-06-03 11:47:45 +0200

    Added extra white space.

--- plugin/swifty-page-manager/js/probe/_probe.wp.js
+++ plugin/swifty-page-manager/js/probe/_probe.wp.js
@@ -1,62 +1,62 @@
 ( function( $, probe ) {
-    probe.WP = probe.WP || {}
+    probe.WP = probe.WP || {};
 
     probe.WP.AdminOpenSubmenu = {
-        Start: function( input ) {
+        Start: function( /*input*/ ) {
             // Check if the WP menu is collapsed (to one icon) ( happens on small screens )
             $( "li#wp-admin-bar-menu-toggle" )
-                .IfVisible().OtherIfNotVisible( "ul#adminmenu" ).Click();
+                .IfVisible()
+                .OtherIfNotVisible( "ul#adminmenu" )
+                .Click();
 
             // Wait until the submenu becomes visible
-            $( "ul#adminmenu" )
-                .WaitForVisible( "Step2" );
+            $( "ul#adminmenu" ).WaitForVisible( "Step2" );
         },
 
         Step2: function( input ) {
             // Click on the menu item in the left admin bar
             $( this.GetSelMainmenu( input.plugin_code ) )
-                .MustExist().Click();
+                .MustExist()
+                .Click();
 
             // Wait until the submenu becomes visible
-            $( this.GetSelSubmenu( input.submenu_text ) )
-                .WaitForFn( "Wait2", "Step3" );
+            $( this.GetSelSubmenu( input.submenu_text ) ).WaitForFn( "Wait2", "Step3" );
         },
 
         Wait2: function( input ) {
             // Trick WP into thinking the mouse hovers over the menu item (so the submenu popup opens)
             // In some cases (WP version, screen size) this hover is needed
-            $( this.GetSelMainmenu( input.plugin_code ) )
-                .AddClass( "opensub" );
+            $( this.GetSelMainmenu( input.plugin_code ) ).AddClass( "opensub" );
 
             // Is the submenu item visible?
-            var check = $( this.GetSelSubmenu( input.submenu_text ) )
-                            .IsVisible();
+            var check = $( this.GetSelSubmenu( input.submenu_text ) ).IsVisible();
+
             return { "wait_result": check };
         },
 
         Step3: function( input ) {
             // Click on the sub menu
             $( this.GetSelSubmenu( input.submenu_text ) )
-                .MustExist().Click();
+                .MustExist()
+                .Click();
         },
 
-        GetSelMainmenu: function( pluginCode) {
+        GetSelMainmenu: function( pluginCode ) {
             return "li#menu-" + pluginCode;
         },
 
-        GetSelSubmenu: function( submenuText) {
+        GetSelSubmenu: function( submenuText ) {
             return "a:contains('" + submenuText + "')";
         }
-    }
+    };
 
     ////////////////////////////////////////
 
     probe.WP.ActivatePlugin = {
         Start: function( input ) { // dorh Not tested
-            $( "a:contains('" + input.s_activate + "')[href*='plugin=" + input.plugin_code + "']" )
-                .Click();
+            $( "a:contains('" + input.s_activate + "')[href*='plugin=" + input.plugin_code + "']" ).Click();
         }
-    }
+    };
 
     ////////////////////////////////////////
 
--- plugin/swifty-page-manager/js/probe/probe.spm.js
+++ plugin/swifty-page-manager/js/probe/probe.spm.js
@@ -1,16 +1,22 @@
 ( function( $, probe ) {
-    probe.SPM = probe.SPM || {}
+    probe.SPM = probe.SPM || {};
 
     probe.SPM.CheckRunning = {
         Start: function( input ) {
-            probe.QueueStory( "WP.AdminOpenSubmenu", { "plugin_code": 'pages', "submenu_text": input.plugin_name }, "Step2" );
+            probe.QueueStory(
+                "WP.AdminOpenSubmenu",
+                {
+                    "plugin_code": "pages",
+                    "submenu_text": input.plugin_name
+                },
+                "Step2"
+            );
         },
 
         Step2: function( input ) {
-            $( "h2:contains('" + input.plugin_name + "')" )
-                .MustExist();
+            $( "h2:contains('" + input.plugin_name + "')" ).MustExist();
         }
-    }
+    };
 
     ////////////////////////////////////////
 
